b34f0717d1f9354ea72750d33f429c52c77bb197
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-01-22 12:30:21 +0000

    Add clojure support, steal someones elses structure

--- init.el
+++ init.el
@@ -1,102 +1,166 @@
-;; init.el --- Emacs configuration
+;;;;
+;; Packages
+;;;;
 
-;; INSTALL PACKAGES
-;; --------------------------------------
-
+;; Define package repositories
 (require 'package)
-
 (add-to-list 'package-archives
        '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
+
+
+;; Load and activate emacs packages. Do this first so that the
+;; packages are loaded before you start trying to modify them.
+;; This also sets the load path.
 (package-initialize)
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar myPackages
-  '(
-    ;; Python
+;; Define he following variables to remove the compile-log warnings
+;; when defining ido-ubiquitous
+;; (defvar ido-cur-item nil)
+;; (defvar ido-default-item nil)
+;; (defvar ido-cur-list nil)
+;; (defvar predicate nil)
+;; (defvar inherit-input-method nil)
+
+;; The packages you want installed. You can also install these
+;; manually with M-x package-install
+;; Add in your own as you wish:
+(defvar my-packages
+  '(;; makes handling lisp expressions much, much easier
+    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+    paredit
+
+    ;; key bindings and code colorization for Clojure
+    ;; https://github.com/clojure-emacs/clojure-mode
+    clojure-mode
+
+    ;; extra syntax highlighting for clojure
+    clojure-mode-extra-font-locking
+
+    ;; integration with a Clojure REPL
+    ;; https://github.com/clojure-emacs/cider
+    cider
+
+    ;; allow ido usage in as many contexts as possible. see
+    ;; customizations/navigation.el line 23 for a description
+    ;; of ido
+    ido-completing-read+
+
+    ;; Enhances M-x to allow easier execution of commands. Provides
+    ;; a filterable list of possible commands in the minibuffer
+    ;; http://www.emacswiki.org/emacs/Smex
+    smex
+
+    ;; project navigation
+    projectile
+
+    ;; colorful parenthesis matching
+    rainbow-delimiters
+
+    ;; edit html tags like sexps
+    tagedit
+
+    ;; because noobs need a file explorer sometimes
+    neotree
+
+    ;; a selection of python things
     better-defaults
     ein
     elpy
     flycheck
     py-autopep8
-    ;; FRONTEND
-    web-mode
-    ;; VUE
-    vue-mode
-    ;; OTHER
-    auto-complete
-    atom-one-dark-theme
-    hackernews
-    neotree
-    rainbow-delimiters
-))
+    
+    ;; git integration
+    magit))
 
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
+;; On OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(if (eq system-type 'darwin)
+    (add-to-list 'my-packages 'exec-path-from-shell))
 
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
-;;(setq inhibit-startup-message t) ;; hide the startup message
 
-(global-linum-mode t) ;; enable line numbers globally
+;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
+;; to load them.
+;;
+;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
+;; then you can add the following code to this file:
+;;
+;; (require 'yaml-mode)
+;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+;; 
+;; Adding this code will make Emacs enter yaml mode whenever you open
+;; a .yml file
+(add-to-list 'load-path "~/.emacs.d/vendor")
 
-(load-theme 'atom-one-dark t) ;; load theme
 
-;; we need to be able to type M-3 to get a hash
-(fset 'insertHash "#")
-(global-set-key (kbd "M-3") 'insertHash)
+;;;;
+;; Customization
+;;;;
 
-;; set font and font size
-(set-face-attribute 'default nil :height 140) ;; 14px
+;; Add a directory to our load path so that when you `load` things
+;; below, Emacs knows where to look for the corresponding file.
+(add-to-list 'load-path "~/.emacs.d/customizations")
 
-;; PYTHON CONFIGURATION
-;; --------------------------------------
+;; Sets up exec-path-from-shell so that Emacs will use the correct
+;; environment variables
+(load "shell-integration.el")
 
-(elpy-enable)
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
+;; These customizations make it easier for you to navigate files,
+;; switch buffers, and choose options from the minibuffer.
+(load "navigation.el")
 
-;; use flycheck not flymake with elpy
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
+(load "ui.el")
 
-;; enable autopep8 formatting on save
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; These customizations make editing a bit nicer.
+(load "editing.el")
 
-;; GUI stuff
+;; Hard-to-categorize customizations
+(load "misc.el")
 
-;; split window into 3 to get cracking
-(defun split-3-windows-horizontally-evenly ()
-  (interactive)
-  (command-execute 'split-window-horizontally)
-  (command-execute 'split-window-horizontally)
-  (command-execute 'balance-windows)
-)
-
-;; no scrollbars, thanks
-(scroll-bar-mode -1)
-
-;; bigger window on load
-(when window-system (set-frame-size (selected-frame) 200 60))
+;; For editing lisps
+(load "elisp-editing.el")
 
 ;; neotree
 (global-set-key [f8] 'neotree-toggle)
 
-;; init.el ends here
+;; Langauage-specific
+(load "setup-clojure.el")
+(load "setup-js.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (atom-one-dark-theme vue-mode py-autopep8 material-theme flycheck elpy ein better-defaults))))
+    (cider-decompile magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
